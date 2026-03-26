2211f79c1bc37b20651c89889523dd7c789ac741
DetsuDev <tomibottelli21@gmail.com>
2026-03-26 20:22:27 -0300

    Agregado Form Principal y Form de Ejercicio 1

---
+++ TP1_GRUPO_21/FormPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP1_GRUPO_21
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1ej ejercicio2Form = new Form1ej(this);
+            ejercicio2Form.Show();
+            this.Hide();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
