89fc61b39d827de2f1f87ccd8251d0a1f1cdec18
Sebastian Fuentes <sebastianfuentesjhi@gmail.com>
2026-04-06 12:53:23 -0300

    comentarios

--- TP1_GRUPO_21/Form2ej.cs
+++ TP1_GRUPO_21/Form2ej.cs
@@ -28,14 +28,14 @@ namespace TP1_GRUPO_21
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            if (!(string.IsNullOrWhiteSpace(txtNombre.Text)) && !(string.IsNullOrWhiteSpace(txtApellido.Text)))
+            if (!(string.IsNullOrWhiteSpace(txtNombre.Text)) && !(string.IsNullOrWhiteSpace(txtApellido.Text))) // "IsNullOrWhiteSpace" retorna "true" si detecta si hay un espacio en blanco o no hay texto, con esto verifico que no se inserten datos no validos
             {
                 listElementos.Items.Add(txtNombre.Text + " " + txtApellido.Text); // Agrega el nombre completo a la lista
 
             }
             else
             {
-                MessageBox.Show("Complete los campos para agregar los elementos a la lista");
+                MessageBox.Show("Complete los campos para agregar los elementos a la lista"); // "si termina habiendo datos invalidos, se muestra un MessageBox con el error"
             }
         }
 
